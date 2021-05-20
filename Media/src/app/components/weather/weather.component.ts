cf6e24e59688b3781593c28233ee1a3ecec8bd48
mrsree9 <84464735+mrsree9@users.noreply.github.com>
2021-05-20 12:10:10 -0700

    weather changes

--- Media/src/app/components/weather/weather.component.ts
+++ Media/src/app/components/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {WeatherService} from "../../services/weather.service";
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  constructor(private weatherService:WeatherService) { }
+  public weather: any ;
+  public date:string = new Date().toLocaleDateString();
+  public weatherImage:string = '';
 
   ngOnInit(): void {
+    this.weatherService.getWeatherDetails().subscribe(weather => {
+      console.log(weather); 
+      this.weather = weather;
+
+      this.weatherImage =`http://openweathermap.org/img/wn/${this.weather.list[0].weather[0].icon}@4x.png`
+    }, err => {
+      console.log(err);
+
+    })
   }
 
 }
--- Media/src/app/services/weather.service.ts
+++ Media/src/app/services/weather.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  weatherURL: string = 'http://localhost:3000/weather'
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { } 
+
+  getWeatherDetails(): Observable<any>{
+    return this.httpClient.get(this.weatherURL);
+  }
 }
