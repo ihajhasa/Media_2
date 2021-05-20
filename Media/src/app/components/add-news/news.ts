06515bb280d2815b32c72d380d552a7b32273741
palashjain2801 <palashjain2801@gmail.com>
2021-05-20 15:04:37 -0500

    update:FininshedAdminSection

---
+++ Media/src/app/components/add-news/news.ts
@@ -0,0 +1,8 @@
+// export class news{
+//     title: string;
+//     description: string;
+//     url : string;
+//     urlToImage: string;
+//     publishedAt : string;
+//     category: string;
+// }
--- Media/src/app/components/news-list/news-list.component.ts
+++ Media/src/app/components/news-list/news-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service';
+// import { news } from '../add-news/news';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-news-list',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-list.component.css']
 })
 export class NewsListComponent implements OnInit {
-
-  constructor() { }
-
+  newslist:any[]=[]
+  constructor(private _newsService:NewsService,private router: Router, private route: ActivatedRoute) { }
   ngOnInit(): void {
+    this._newsService.getAllNews().subscribe(
+      data=>{
+        this.newslist = data;
+
+      }
+    )
+    
+  }
+  deleteme(id:any){
+    console.log('deleteme is called ',id._id)
+    this._newsService.deleteNews(id._id).subscribe(
+      data=>{
+        this._newsService.getAllNews().subscribe(
+          data=>{
+            this.newslist = data;
+          }
+        )
+      }
+    )
+    this.router.navigate(['/admin']);
+  }
+  editme(editnews:any){
+    console.log('edit is called for id', editnews._id);
+    this.router.navigate(['/admin/edit-news',editnews._id])
   }
 
 }
