48002ec806d61de4a94d565fc3500409b8a70ce9
Ishaq Yousef <TCS@Ishaqs-MacBook-Pro.local>
2021-05-21 14:15:38 -0400

    Fixed Chat Component

--- Media/src/app/components/chat/chat.component.ts
+++ Media/src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  updateNickname() {
+    this.socket.emit('nick', this.nickname);
+  }
+
+  sendMessage() {
+    console.log(this.message)
+    this.socket.emit('chat', {message: this.message});
+    this.message = ""
+  }
+
+  nickname: string = ""
+  SOCKET_ENDPOINT: string = "localhost:3000";
+  chat: string = ""
+  socket: any;
+
+  message: string = ""
+
   constructor() { }
 
   ngOnInit(): void {
+    this.socket = io(this.SOCKET_ENDPOINT, { transports : ['websocket'] })
+    this.socket.on('chat', (data: any) => {
+      this.chat = this.chat + data + '\n'
+    })
   }
 
 }
+
