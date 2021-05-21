447d0e88387e9c5647e6206a9811c8c2f433ad20
Jeffrey-Chau <Jeffrey-Chau@users.noreply.github.com>
2021-05-21 09:50:57 -0700

    chat

--- API/userapi/routes/userChat.js
+++ API/userapi/routes/userChat.js
@@ -3,25 +3,12 @@ const path = require('path');
 const http = require('http');
  
 const app = express();
-app.set('port', process.env.PORT || 9900);
+//app.set('port', process.env.PORT || 4300);
 app.use(express.static(path.join(__dirname, 'public')));
 
-const server = http.createServer(app).listen(app.get('port'), () => {
-    console.log("Express server listening on port " + app.get('port'));
-});
-const io = require('socket.io').listen(server);
+const io = require('socket.io')(4300);
 
-// connection event
-// socket represents each client connected to our server
 io.on('connection',  (socket) => {
-
-    socket.on('connect', ()=>{
-        console.log("New connection socket.id : ", socket.id)
-    })
-
-    socket.on('disconnect', ()=>{
-        console.log("disconnect => nickname : ", socket.nickname)
-    })
 
     // nick event
     socket.on('nick', (nickname) => {
