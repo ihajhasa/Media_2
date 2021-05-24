03f7934851251bf8ed77a89ebb868ddae87b9056
Jeffrey-Chau <Jeffrey-Chau@users.noreply.github.com>
2021-05-24 14:17:51 -0700

    working contact us

--- Media/src/app/components/contact-us/contact-us.component.ts
+++ Media/src/app/components/contact-us/contact-us.component.ts
@@ -9,29 +9,33 @@ import { Contact } from './contact';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-
-  constructor(private flashMessage: FlashMessagesService,private contactservice: ContactusService) { }
-
+  
+  contactName!:string;
+  contactEmail!:string;
+  contactSubject!:string;
+  contactMessage!:string;
   contact : Contact = {
     email: '',
     message:'',
     name:'',
     subject:'',
   }
+  
+  constructor(private flashMessage: FlashMessagesService,private contactservice: ContactusService) { }
+
   ngOnInit(): void {
   }
+  
 add(){
   this.contact ={
-    email: '  ',
-    message:' ',
-    name:' ',
-    subject:' ',
-    
+    email: this.contactEmail,
+    message: this.contactMessage,
+    name: this.contactName,
+    subject: this.contactSubject,
   }
 }
 
   showFlash() {
-    
     this.flashMessage.show('Your request has been sent successfully', { cssClass: 'alert-success', timeout: 1000 });
     this.add();
     this.sendmessage();
